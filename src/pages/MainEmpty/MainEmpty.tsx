2a0cc6234fb0cc53f3d79bd977083396cd83c966
chumakov.aa <chumakov.aa@gazprom-neft.ru>
2024-10-01 00:41:51 +0300

    added other pages from markup

--- src/pages/MainEmpty/MainEmpty.tsx
+++ src/pages/MainEmpty/MainEmpty.tsx
@@ -1,11 +1,4 @@
-import React from 'react';
-import {PlaceCard, PlaceCardProps} from '../../components/PlaceCard/PlaceCard.tsx';
-
-type MainProps = {
-  places: PlaceCardProps[];
-};
-
-export const Main: React.FC<MainProps> = ({places}) => (
+export const MainEmpty = () => (
   <div className="page page--gray page--main">
     <header className="header">
       <div className="container">
@@ -36,7 +29,7 @@ export const Main: React.FC<MainProps> = ({places}) => (
       </div>
     </header>
 
-    <main className="page__main page__main--index">
+    <main className="page__main page__main--index page__main--index-empty">
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
         <section className="locations container">
@@ -57,7 +50,7 @@ export const Main: React.FC<MainProps> = ({places}) => (
               </a>
             </li>
             <li className="locations__item">
-              <a className="locations__item-link tabs__item tabs__item--active">
+              <a className="locations__item-link tabs__item">
                 <span>Amsterdam</span>
               </a>
             </li>
@@ -67,7 +60,7 @@ export const Main: React.FC<MainProps> = ({places}) => (
               </a>
             </li>
             <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
+              <a className="locations__item-link tabs__item tabs__item--active" href="#">
                 <span>Dusseldorf</span>
               </a>
             </li>
@@ -75,32 +68,14 @@ export const Main: React.FC<MainProps> = ({places}) => (
         </section>
       </div>
       <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">312 places to stay in Amsterdam</b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                <svg className="places__sorting-arrow" width="7" height="4">
-                  <use xlinkHref="#icon-arrow-select"/>
-                </svg>
-              </span>
-              <ul className="places__options places__options--custom places__options--opened">
-                <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                <li className="places__option" tabIndex={0}>Price: low to high</li>
-                <li className="places__option" tabIndex={0}>Price: high to low</li>
-                <li className="places__option" tabIndex={0}>Top rated first</li>
-              </ul>
-            </form>
-            <div className="cities__places-list places__list tabs__content">
-              { places.map((place) => (<PlaceCard key={place.name} {...place} />)) }
+        <div className="cities__places-container cities__places-container--empty container">
+          <section className="cities__no-places">
+            <div className="cities__status-wrapper tabs__content">
+              <b className="cities__status">No places to stay available</b>
+              <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
             </div>
           </section>
-          <div className="cities__right-section">
-            <section className="cities__map map"></section>
-          </div>
+          <div className="cities__right-section"></div>
         </div>
       </div>
     </main>
