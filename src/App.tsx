16bde648de1fe3a87512d6993241010e6c509d2d
chumakov.aa <chumakov.aa@gazprom-neft.ru>
2024-10-08 20:22:31 +0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Main } from './pages/Main/Main.tsx';
-import { PlaceCardProps } from './components/PlaceCard/PlaceCard.tsx';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Login} from './pages/Login/Login.tsx';
+import {Favorites} from './pages/Favorites/Favorites.tsx';
+import {Offer} from './pages/Offer/Offer.tsx';
+import {NotFound} from './pages/NotFoundPage/NotFound.tsx';
+import {AppRoute} from './utils/const.ts';
+import {PrivateRoute} from './PrivateRoute.tsx';
+import {TPlaceCard} from './utils/types.ts';
 
-type AppScreenProps = {
-  places: PlaceCardProps[];
+interface IAppProps {
+  places: TPlaceCard[];
 }
 
-export const App: React.FC<AppScreenProps> = ({places}) => (
-  <Main places={places}/>
-);
+export const App: React.FC<IAppProps> = ({places}): JSX.Element => {
+  const isAuthenticated = false;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<Main places={places} />} />
+        <Route path={AppRoute.Login} element={<Login />} />
+        <Route path={AppRoute.Favorites} element={
+          <PrivateRoute isAuthenticated={isAuthenticated}>
+            <Favorites />
+          </PrivateRoute>
+        }
+        />
+        <Route path={AppRoute.Offer} element={<Offer />} />
+        <Route path={'*'} element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
